0ebd439084349fb4d22ad4c0f22526eadd6322a3
MathisGheri <mathis.gheri@epitech.eu>
2024-06-18 19:34:13 +0200

    [+] Save to keep the light turning dark like the night

--- shaders.cpp
+++ shaders.cpp
@@ -187,3 +187,81 @@ int main(void)
 
     return 0;
 }
+
+
+/*
+void GUI::run()
+{
+    WindowPtr window = Window::getInstance();
+    window->setLogInfo("LOG_INFO");
+    window->initWindow(1920, 1080, "raylib [core] example - 3d camera free", 144);
+    CameraWrapper camera;
+    MapPtr map = Map::getInstance();
+    map->setMapSize(10, 10);
+    window->loadModels();
+    float updateInterval = 1.0f / _freq;
+    float lastUpdateTime = GetTime();
+    map->addResource(0, 0, Ressources::RessourceType::FOOD, 1);
+    map->addResource(0, 0, Ressources::RessourceType::LINEMATE, 1);
+    map->addResource(0, 0, Ressources::RessourceType::DERAUMERE, 1);
+    map->addResource(0, 0, Ressources::RessourceType::SIBUR, 1);
+    map->addResource(0, 0, Ressources::RessourceType::MENDIANE, 1);
+    map->addResource(0, 0, Ressources::RessourceType::PHIRAS, 1);
+    map->addResource(0, 0, Ressources::RessourceType::THYSTAME, 1);
+
+    Shader shader = LoadShader("assets/lighting.vs", "assets/lighting.fs");
+    shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
+    int ambientLoc = GetShaderLocation(shader, "ambient");
+    float ambient[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
+    SetShaderValue(shader, ambientLoc, ambient, SHADER_UNIFORM_VEC4);
+    std::vector<Model> _loadedModels = window->getLoadedModels();
+    std::vector<Model> resourceModels = window->getResourceModels();
+    for (int i = 0; i < _loadedModels.size(); i++) {
+        for (int j = 0; j < _loadedModels[i].materialCount; j++) {
+            _loadedModels[i].materials[j].shader = shader;
+        }
+    }
+    for (int i = 0; i < resourceModels.size(); i++) {
+        for (int j = 0; j < resourceModels[i].materialCount; j++) {
+            resourceModels[i].materials[j].shader = shader;
+        }
+    }
+    Light lights[MAX_LIGHTS] = { 0 };
+    lights[0] = CreateLight(LIGHT_POINT, (Vector3){ 0, 1, 0 }, Vector3Zero(), WHITE, shader); // Initialiser la lumière à blanche
+
+    float dayDuration = 60.0f; // Durée d'un cycle jour-nuit en secondes
+
+    while (!WindowShouldClose())
+    {
+        float currentTime = GetTime();
+        float timeOfDay = fmod(currentTime, dayDuration) / dayDuration; // Normaliser le temps entre 0 et 1
+
+        // Calculer la couleur de la lumière en fonction du temps de la journée
+        float lightIntensity = 1.0f - timeOfDay; // Passe de 1.0 (plein jour) à 0.0 (nuit complète)
+        Color lightColor = Color{ (unsigned char)(255 * lightIntensity), (unsigned char)(255 * lightIntensity), (unsigned char)(255 * lightIntensity), 255 };
+
+        // Mettre à jour la lumière
+        lights[0].color = lightColor;
+        UpdateLightValues(shader, lights[0]);
+
+        camera.update();
+        float cameraPos[3] = { camera.getX(), camera.getY(), camera.getZ() };
+        SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
+        for (int i = 0; i < MAX_LIGHTS; i++) UpdateLightValues(shader, lights[i]);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        camera.BeginMode();
+        window->updateMap(map);
+        window->DrawMap(map->getMapSizeX(), map->getMapSizeY());
+        for (int i = 0; i < MAX_LIGHTS; i++)
+        {
+            if (lights[i].enabled) DrawSphereEx(lights[i].position, 0.2f, 8, 8, lights[i].color);
+        }
+        camera.EndMode();
+        window->DrawFps(10, 10);
+        EndDrawing();
+    }
+    CloseWindow(); 
+    exit(0);
+}*/
