9406a3ddce79ec13126b81049a86e45154fc63ef
MathisGheri <mathis.gheri@epitech.eu>
2024-06-25 16:56:37 +0200

    [+] Need to fix the command with the server

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,20 +6,34 @@
 */
 
 #include "GameEngine.hpp"
+#include "Server.hpp"
 
 int main(int argc, char **argv)
 {
-    GameEngine gameEngine;
-     if (argc != 5) {
+    if (argc != 5) {
         std::cerr << "USAGE: " << argv[0] << " -p port -h machine" << std::endl;
         return 1;
     }
     std::string port = argv[2];
     std::string host = argv[4];
+
+    GameEngine gameEngine;
+    Server server;
+    if (server.connect_server(port, host) != 0) {
+        std::cerr << "Failed to connect to server" << std::endl;
+        return 1;
+    }
+    // gameEngine.Initialize();
+
+    std::thread listeningThread(&Server::listening, &server);
+    std::thread gameThread(&GameEngine::Run, &gameEngine);
     
-    gameEngine.Initialize();
-    gameEngine.Run();
+    // gameEngine.Run();
+    // gameEngine.Shutdown();
+
+    gameThread.join();
     gameEngine.Shutdown();
+    listeningThread.join(); 
 
     return 0;
 }
